ea90ceff0833c9f96cd22c0ea0a0190137f3a0a3
AleFalcone27 <83794890+AleFalcone27@users.noreply.github.com>
2023-03-22 22:09:15 -0300

    23/03/23

--- Tracker/tracker_main.py
+++ Tracker/tracker_main.py
@@ -1,88 +1,79 @@
 import dearpygui.dearpygui as dpg
+from datetime import date
+from openpyxl import Workbook, load_workbook
 
-# generamos las categorias 
-categorias = ["Comida","Regalos","Compra Super","Ingresos"]; 
+dpg.create_context() # Creamos contexto para poder acceder a la libreria 
 
-# generamos tuplas para discriminar los gatos por categorias
-comida = []
-regalos = []
-compra_super = []
-ingresos = []
+dpg.create_viewport(title='Custom Title', width=600, height=300) # Damos el nombre y medidas a la ventana
 
-# cargamos los montos ingresados en cada una de las caracteristicas 
-def cargar(categoria_elegida,precio_ingresado):
+# Generamos las categorias
+categorias = ["Comida","Regalos","Compra Super","Ingresos"]
 
-    # Hacerlo con excel 
+# Seteamos la bandera en True
+bandera = True;
+
+# Obtenemos la fecha
+fecha = date.today()
+
+
+# Realizamos la carga de datos en el excel
+def cargar(fecha,categoria_ingresada,precio_ingresado,comentario_ingresado):
+    heading = ["fecha","categoria","precio","comentario"]
+
+    bandera = True
+
+    # Cargamos el archivo 
+    wb = load_workbook("gastos.xlsx")
+
+    # Agarramos la pestaña activa 
+    ws = wb.active
+
+    # Los headers se van a escribir solo la primera vez que corra el codigo
+    while bandera == True:
+        ws.append(heading)
+        bandera = False;
+
+    # cargamos en el archivo el nuevo gasto
+    ws.append([fecha,categoria_ingresada,precio_ingresado,comentario_ingresado])
+
+    # Save the file
+    wb.save("gastos.xlsx")
+
     
-    if(categoria_elegida == "Regalos"):
-        print("REGALOS")
-        regalos.append(precio_ingresado) 
-        
-    elif(categoria_elegida == "Comida" ):
-        print("COMIDA")
-        comida.append(precio_ingresado)
-        
-    elif(categoria_elegida == "Compra Super"):
-        print("Compra super")
-        compra_super.append(precio_ingresado)
-        
-    elif(categoria_elegida == "Ingresos"):
-        print("ingresos")
-        
-        ingresos.append(precio_ingresado)
-        
-
-def button_callback():
-    
+def save():
     precio_ingresado = dpg.get_value(precio)
+    categoria_ingresada = dpg.get_value(categoria)
+    comentario_ingresado = dpg.get_value(comentario)
+
+    print(precio_ingresado)
+    print(categoria_ingresada)
+    print(comentario_ingresado)
     
-    print("PRECIO: " + precio_ingresado)
+    cargar(fecha,categoria_ingresada,precio_ingresado,comentario_ingresado)
+
+
+with dpg.window(label="Example Window"):
+    dpg.add_text("Hello, world")
+    dpg.add_button(label="Save", callback= save,)
     
-    dpg.set_value(precio, "PRECIO"); #seteamos el valor a PRECIO para preparar la siguiente carga
+    precio = dpg.add_input_text(label="PRECIO",tag=1); # Cramos input del precio
     
-    categoria_elegida = dpg.get_value(categoria) # obtenemos la categoria
-
-    dpg.set_value(categoria, "CATEGORIA") #seteamos el valor a CATEGORIAS para preparar la siguiente carga
+    categoria = dpg.add_combo(categorias, tag=2, label="CATEGORIAS"); # Creamos categoria del gasto
     
-    cargar(categoria_elegida,precio_ingresado)
-
-        
-
-
-dpg.create_context() #creamos el contexto para acceder los comandos de la libreria
-
-
-
-with dpg.window(label="aa"):
-    dpg.add_text("VAMOS A TRAKEAR TUS GASTOS MENSUALES")
-    dpg.add_button(label="INGRESAR", callback= button_callback, tag="boton") # boton para cargar los datos 
-    
-    precio = dpg.add_input_text(tag=1); # input del precio
-    
-    categoria = dpg.add_combo(categorias, tag=2); # categoria del gasto
-    dpg.set_value(categoria, "CATEGORIA")
-
+    comentario = dpg.add_input_text(label="COMENTARIO", tag=3) # Creamos input del comentario
     
     
-    
-    
-    
-# crea la ventana grafica
-dpg.create_viewport(title='NLAFM', width=1200, height=600) # nombre de ka ventana , ancho , alto
-
-dpg.setup_dearpygui()
-
-dpg.show_viewport() # mostramos la ventana grafica
-
-dpg.start_dearpygui() # arranca el bucle de renderizado
-
-
-print(comida)
-print(regalos)
-print(compra_super)
-print(ingresos)
 
 
 
-dpg.destroy_context() #Destruimos el contexto
+
+
+
+
+
+dpg.setup_dearpygui() 
+dpg.show_viewport() # Mostramos la ventana grafica
+dpg.start_dearpygui() # Comienza el bucle de renderizado
+
+dpg.destroy_context() # Destruimos el contexto
 
